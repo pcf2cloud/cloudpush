3267b6ecc9bc959720584d8e0133367fa80cde48
PCF <40490811+pcf2cloud@users.noreply.github.com>
2018-08-03 17:08:42 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+// Controller
 @RestController
 @RequestMapping("/cloud")
 public class UserController {
