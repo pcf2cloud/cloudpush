34cfe97d768caca46715c75f858ee9af62ee6ce7
PCF <40490811+pcf2cloud@users.noreply.github.com>
2018-08-03 16:59:33 +0530

    Update UserController.java
    code changes

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 	private UserService userservice;
+	ArrayList<User> al=new ArrayList<User>();  
 
 	@Autowired
 	public UserController(UserService userservice) {
@@ -21,6 +24,16 @@ public class UserController {
 	public @ResponseBody User getUser() {
 		User u =new  User("POI000987", "Manvendra Singh");
 		return userservice.getUser(u);
+
+	}
+	
+		@RequestMapping("/all")
+	public @ResponseBody List<User> getAllUser() {
+		User u1 =new  User("POI000987", "WIPRO");
+		User u2 =new  User("POI000981", "TCS");
+		al.add(u1);
+		al.add(u2);
+		return al;
 
 	}
 	
